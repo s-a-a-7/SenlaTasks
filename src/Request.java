af00605a4237f922496a8de011bf81ede4f13c71
Ann <baletkosmos@gmail.com>
2025-11-03 00:30:10 +0300

    Request список запросов и его сортировка + addOrder, completeOrder, сhangeQuantRequest

--- src/Request.java
+++ src/Request.java
@@ -1,20 +1,36 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Request {
     private boolean statusRequest = false;
+
+    private List<Book> requestList = new ArrayList<>();
 
     Order order = new Order();
 
     public void addRequest(Book book, Warehouse warehouse){
-        order.setStatus("новый");
+        order.addOrder(order, book);
         System.out.println(order.getStatus() + " заказ на книгу: " + "«" + book.getName() +  "»");
 
         if (warehouse.isBookExist(book) && book.getStatus()){
             book.changeQuantity(-1);
-            order.setStatus("выполнен");
+
+            order.completeOrder(order);
             System.out.println("заказ " + order.getStatus());
         }
         else{
             System.out.println("книга отстутствует, сформирован запрос на книгу");
             this.statusRequest = true;
+
+            if (requestList.contains(book)){
+                requestList.add(book);
+                book.changeQuantRequest(statusRequest);
+            }
+            else{
+                book.changeQuantRequest(statusRequest);
+            }
+
         }
     }
 
@@ -29,9 +45,12 @@ public class Request {
         if (statusRequest){
             if (warehouse.isBookExist(book) && book.getStatus()){
                 statusRequest = false;
+                book.changeQuantRequest(statusRequest);
+
                 System.out.println("книга " + "«" + book.getName() +  "»" + " появилась");
                 book.changeQuantity(-1);
-                order.setStatus("выполнен");
+
+                order.completeOrder(order);
                 System.out.println(order.getStatus() + " заказ на книгу: " + "«" + book.getName() +  "»");
             }
             else{
@@ -40,4 +59,30 @@ public class Request {
         }
     }
 
+    public List<Book> getRequestList(){
+        return requestList;
+    }
+    public void printRequestList(){
+        System.out.println();
+        System.out.println("▷ Список запросов на книги:");
+        for (Book book : requestList) {
+            System.out.print("   кол-во запросов: " + book.getQuantRequest());
+            System.out.print(" | на книгу: " + "«" + book.getName() +  "»");
+        }
+        System.out.println("\n");
+    }
+
+}
+
+class RequestNameComparator implements Comparator<Book> {
+    @Override
+    public int compare(Book a, Book b) {
+        return a.getName().compareTo(b.getName());
+    }
+}
+class RequestQuantComparator implements Comparator<Book> {
+    @Override
+    public int compare(Book a, Book b) {
+        return a.getQuantRequest() - b.getQuantRequest();
+    }
 }
