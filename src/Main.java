e4baf5362bbc82ebccf32608853ebb9a4d846bb0
Ann <baletkosmos@gmail.com>
2025-11-03 18:06:37 +0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public static void main(String[] args){
     Book book1 = new Book("Шерлок Хомс", 2021, 400);
-    Book book2 = new Book("Зло под солнцем", 2021, 390);
+    Book book2 = new Book("Зло под солнцем", 2021, 370);
     Book book3 = new Book("Убийство по алфавиту", 2022, 390);
+
+    Description description = new Description();
+    description.getDescription(PossibleBooks.EvilUnderSun);
 
     Warehouse warehouse = new Warehouse();
     warehouse.addBook(book1);
@@ -9,20 +12,40 @@ public static void main(String[] args){
     warehouse.addBook(book2);
     warehouse.addBook(book3);
 
-    warehouse.printBooks();
-
     Request request = new Request();
-    request.addRequest(book1, warehouse);
-    request.addRequest(book2, warehouse);
-    request.addRequest(book2, warehouse);
-    request.addRequest(book2, warehouse);
-    request.addRequest(book3, warehouse);
-    request.addRequest(book3, warehouse);
+    OrderList orderList = new OrderList();
 
-    warehouse.addBook(book3);
+    request.addRequest(book1, warehouse, orderList);
+    request.addRequest(book2, warehouse, orderList);
+    request.addRequest(book2, warehouse, orderList);
+    request.addRequest(book2, warehouse, orderList);
+    request.addRequest(book3, warehouse, orderList);
+    request.addRequest(book3, warehouse, orderList);
+    request.addRequest(book3, warehouse, orderList);
 
-    request.recheckRequest(book2, warehouse);
-    request.recheckRequest(book3, warehouse);
+    OrderDetails orderDetails = new OrderDetails();
+    orderDetails.getOrderDetails(orderList);
+
+//    warehouse.addBook(book3);
+
+//    request.recheckRequest(book2, warehouse, orderList);
+//    request.recheckRequest(book3, warehouse, orderList);
+
+    Comparator<Order> orderComparator = new DateOrderComparator().thenComparing(new PriceOrderComparator());
+//    orderList.getOrders().sort(orderComparator.thenComparing(new StatusOrderComparator()));
+//    orderList.getOrdersCompleted().sort(orderComparator);
+
+    orderList.printOrders();
+    orderList.printOrdersCompleted();
+
+
+//    request.getRequestList().sort(new RequestQuantComparator().thenComparing(new RequestNameComparator()));
+
+    request.printRequestList();
+
+    Comparator<Book> warehouseComparator = new BookNameComparator().thenComparing(new BookYearComparator()).
+            thenComparing(new BookPriceComparator()).thenComparing(new BookStatusComparator());
+//    warehouse.getBooks().sort(warehouseComparator);
 
     warehouse.printBooks();
 }
