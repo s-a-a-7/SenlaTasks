dfbeefc28091389fead3d5af71de64df55b5310c
Ann <baletkosmos@gmail.com>
2025-11-07 13:55:18 +0300

    update: удаление старого

--- src/OrderDetails.java
+++
@@ -1,6 +0,0 @@
-public class OrderDetails {
-    public void getOrderDetails(OrderList orderList){
-        System.out.println("\n  Номер Вашего заказа: " + orderList.getOrderNum());
-        orderList.printOrdersBook();
-    }
-}
--- src/PossibleBooks.java
+++
@@ -1,5 +0,0 @@
-public enum PossibleBooks{
-    SherlockHoms,
-    EvilUnderSun,
-    AlphabeticalMurder
-}
